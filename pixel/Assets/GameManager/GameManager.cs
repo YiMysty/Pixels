13344e1e7ce23dab0080e42e64bbf38222c508f6
YiMysty <yimingzi@foxmail.com>
2015-02-06 15:46:02 -0600

    Change the view. And Game start and end event.

---
+++ pixel/Assets/GameManager/GameManager.cs
@@ -0,0 +1,15 @@
+﻿public static class GameManager{
+	public delegate void GameEvent();
+	public static event GameEvent GameStart,GameOVer;
+	public static void TriggerGameStart(){
+		if (GameStart != null) {
+			GameStart();
+		}
+	}
+
+	public static void TriggerGameOver(){
+		if (GameOVer != null) {
+			GameOVer ();
+		}
+	}
+}
